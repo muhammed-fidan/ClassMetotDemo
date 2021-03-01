d161119392248174ab756a07a50cfe682c04b0f0
Muhammed Fidan <Muhammed Fidan@LAPTOP-VIMI1TDU>
2021-03-01 19:42:05 +0400

    3.Ders Ödevi

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.MusteriEkleme();
+            musteriManager.MusteriListeleme();
+            musteriManager.MusteriSilme();
+        }
+
+        class Musteri
+        {
+            public int CustomerId { get; set; }
+            public string CustomerName { get; set; }
+            public string CustomerLastName { get; set; }
+
+        }
+
+        class MusteriManager
+        {
+            public void MusteriEkleme()
+            {
+                Console.WriteLine("Müşteri Başarıyla Eklendi");
+            }
+
+            public void MusteriListeleme()
+            {
+                Console.WriteLine("Müşteriler Başarıyla Listelendi");
+            }
+
+            public void MusteriSilme()
+            {
+                Console.WriteLine("Müşteri Başarıyla Silindi");
+            }
+        }
+    }
+}
